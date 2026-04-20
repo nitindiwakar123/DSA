b89c2d71acbbb5af85aadc20455db018a2dc7542
nitindiwakar123 <nitindiwakar2006@gmail.com>
2026-04-20 14:14:09 +0530

    add cube programs

--- 05Loops/whileLoop/assignment1.js
+++ 05Loops/whileLoop/assignment1.js
@@ -31,4 +31,69 @@ function printSquare(n) {
     }
 }
 
-printSquare(10)
+// printSquare(10);
+
+// WAP to print all the numbers from 1 to 100 which are perfect square.
+
+function printPerfectSquare(n) {
+    let i = 1;
+    while (i*i<=n) {
+        console.log(i*i);
+        i++;
+    }
+}
+
+// printPerfectSquare(10);
+
+// WAP to print and count all the numbers from 1 to 100 which are perfect square.
+
+function printAndCountPerfectSquare(n) {
+    let i = 1;
+    let count = 0;
+    while (i*i<=n) {
+        console.log(i*i);
+        count++;
+        i++;
+    }
+
+    console.log(`Count is: ${count}`);
+}
+
+// printAndCountPerfectSquare(10);
+
+// WAP to print cube of each numbers from 1 to 100.
+function printCubeOfEach(n) {
+    let i = 1;
+    while (i<=n) {
+        console.log(i*i*i);
+        i++;
+    }
+}
+
+// printCubeOfEach(100);
+
+// WAP to print all the numbers from 1 to 100 which are perfect cube.
+function printPerfectCube(n) {
+    let i = 1;
+    while (i**3<=n) {
+        console.log(i**3); // i**3 = i*i*i
+        i++;
+    }
+}
+
+// printPerfectCube(100);
+
+// WAP to print and count all the numbers from 1 to 100 which are perfect cube.
+function printAndCountPerfectCube(n) {
+    let i = 1;
+    let count = 0;
+    while (i**3<=n) {
+        console.log(i**3); // i**3 = i*i*i
+        count++;
+        i++;
+    }
+    console.log(`Count is: ${count}`);
+}
+
+// printAndCountPerfectCube(1000);
+
