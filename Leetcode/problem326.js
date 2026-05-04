d436fae7c097a2a5014678862a22787cc7a78092
nitindiwakar123 <nitindiwakar2006@gmail.com>
2026-05-04 16:27:32 +0530

    add more solutions of problem326

--- Leetcode/problem326.js
+++ Leetcode/problem326.js
@@ -1,24 +1,46 @@
-function powerOfThree(n) {
-    if (n == 1) return true;
+// function powerOfThree(n) {
+//     if (n == 1) return true;
 
-    if (n > 0 && n % 3 == 0) {
-        while (n > 1) {
-            n = n / 3;
-        }
-        return n == 1;
-    }
+//     if (n > 0 && n % 3 == 0) {
+//         while (n > 1) {
+//             n = n / 3;
+//         }
+//         return n == 1;
+//     }
 
-    return false;
-}
+//     return false;
+// }
+
+// Clean Aproach - 1
+// function powerOfThree(n) {
+//     if(n <= 0) return false;
+//     while (n > 1) {
+//         if ((n % 3 )!= 0) return false;
+//         n = n / 3;
+//     }
+//     return true;
+// }
+
+// Clean Aproach - 2 - Not Recommended
+// function powerOfThree(n) {
+//     while (n > 1) {
+//         n = n / 3;
+//     }
+//     return n==1;
+// }
 
 // Test Cases
-// console.log(powerOfThree(-1));
-// console.log(powerOfThree(0));
-// console.log(powerOfThree(27));
-// console.log(powerOfThree(81));
-// console.log(powerOfThree(243));
+console.log(powerOfThree(6));
+console.log(powerOfThree(-1));
+console.log(powerOfThree(0));
+console.log(powerOfThree(27));
+console.log(powerOfThree(81));
+console.log(powerOfThree(243));
 // console.log(powerOfThree(59049));
-console.log(powerOfThree(1));
+// console.log(powerOfThree(21));
+// console.log(powerOfThree(15));
+// console.log(powerOfThree(18));
+// console.log(powerOfThree(1));
 // console.log(powerOfThree(10));
 // console.log(powerOfThree(45));
 // console.log(powerOfThree(-27));
