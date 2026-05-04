0430c334366545c5140d3cb959be6e6123ae4c3f
nitindiwakar123 <nitindiwakar2006@gmail.com>
2026-05-04 16:56:33 +0530

    add more solutions of problem342

--- Leetcode/problem342.js
+++ Leetcode/problem342.js
@@ -1,8 +1,30 @@
+// function is4sPower(n){
+//     while (n > 1) {
+//         n = n / 4;
+//     }   
+//     return n==1;
+// };
+
+// More optimized aproach
 function is4sPower(n) {
+    if(n<=0) return false;
     while (n > 1) {
+        if((n%4) != 0) return false;
         n = n / 4;
     }
-    return n==1;
+    return true;
 }
 
-console.log(is4sPower(16));
+// Test Cases
+// console.log(is4sPower(-1));
+// console.log(is4sPower(0));
+// console.log(is4sPower(1));
+// console.log(is4sPower(2));
+// console.log(is4sPower(4));
+// console.log(is4sPower(8));
+// console.log(is4sPower(16));
+// console.log(is4sPower(32));
+// console.log(is4sPower(7));
+// console.log(is4sPower(20));
+// console.log(is4sPower(64));
+// console.log(is4sPower(256));
