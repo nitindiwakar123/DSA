caf1e35c6e5fc04b377aa691591e12598596ddc5
nitindiwakar123 <nitindiwakar2006@gmail.com>
2026-05-13 19:47:56 +0530

    add nthPalindrome program

--- NumberBasedProgramming/numbers1.js
+++ NumberBasedProgramming/numbers1.js
@@ -1,11 +1,3 @@
-function isPrime(n) {
-    if (n < 2) return false;
-    for (let i = 1; i * i <= n; i++) {
-        if (n % i == 0) return false;
-    }
-    return true;
-}
-
 // WAJP to take user input and print all the prime factors of the number.
 function printPrimeFactors(n) {
     let prime = 2;
@@ -79,6 +71,275 @@ function getFactors3(n) {
     }
     console.log("Count:", count);
 }
+
 // getFactors1(12);
 // getFactors2(12);
 // getFactors3(36);
+
+// WAJP to print and count all the perfect numbers in the given range.
+function isPerfectNumber(num) {
+    let sum = 1
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            sum += i;
+            const secondFactor = num / i;
+            if (secondFactor != i) {
+                sum += secondFactor;
+            }
+        }
+    }
+    return (num < 2) ? false : sum == num;
+}
+
+function printAndCountPerfectNumbersInRange1(start, end) {
+    let count = 0;
+    for (let i = start; i<=end; i++) {
+        if (isPerfectNumber(i)) {
+            console.log(i);
+            count++;
+        }
+    }
+
+    console.log("Count: ", count);
+}
+
+// Optimized
+function printAndCountPerfectNumbersInRange2(start, end) {
+    const perfects = [6, 28, 496, 8128];
+
+    for (const number of perfects) {
+        if (number > start && number <= end) {
+            console.log(number);
+        }
+    }
+}
+
+// printAndCountPerfectNumbersInRange2(1, 100);
+
+// WAJP to print kth perfect number.
+
+// Optimized
+function printKthPerfectNumber(k) {
+    const perfectNumbers = [
+        "6",
+        "28",
+        "496",
+        "8128",
+        "33550336",
+        "8589869056",
+        "137438691328",
+        "2305843008139952128",
+        "2658455991569831744654692615953842176",
+        "191561942608236107294793378084303638130997321548169216",
+        "13164036458569648337239753460458722910223472318386943117783728128",
+        "14474011154664524427946373126085988481573677491474835889066354349131199152128"
+    ];
+
+    console.log(perfectNumbers[k - 1]);
+}
+
+function printKthPerfectNumber1(k) {
+    let count = 0;
+    for (let i = 1;i<10000;i++) {
+        if(isPerfectNumber(i)) {
+            count++;
+            if(count==k) {
+                console.log(i);
+                break;
+            }
+        }
+    }
+}
+// printKthPerfectNumber(1);
+
+// WAJP to take user input and print whether the number is Prime number or not.
+// Worst performance
+function isPrime1(n) {
+    if(n==0 || n==1) return false;
+    for (let i = 2; i < n; i++) {
+        if(n%i == 0) return false;
+    }
+    return true;
+}
+
+// Better than first one total iteration = n/2
+function isPrime2(n) {
+    if(n==0 || n==1) return false;
+    for (let i = 2; i <= n/2; i++) {
+        if(n%i == 0) return false;
+    }
+    return true;
+}
+
+// Most Optimized
+function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+// console.log(isPrime3(2));
+
+// WAJP to print and count all the Prime numbers up to a given range.
+function printAndCountPrimesInRange1(start, end) {
+    let count = 0;
+    for (let i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            console.log(i);
+            count++
+        }
+    }
+    console.log(`Count: ${count}`);
+}
+
+function printAndCountPrimesInRange2(start, end) {
+    let count = 0;
+     if (start == 1 || start == 2) {
+        count++;
+        console.log(2);
+     }
+    if (start % 2 == 0)
+        start++;
+   
+    for (let i = start; i <= end; i += 2) {
+        if (isPrime(i)) {
+            console.log(i);
+            count++;
+        }
+    }
+    console.log("Count: ", count);
+}
+
+// printAndCountPrimesInRange2(2, 20);
+
+// WAJP to take user input and print nth prime number.
+function getKthPrime(k) {
+    if (k < 1) return -1;
+    if (k == 1) return 2;
+    let count = 1;
+    let i = 3;
+    while (true) {
+        if (isPrime(i)) {
+            count++;
+            if (count == k)
+                return i;
+        }
+        i += 2;
+    }
+}
+
+// console.log(getKthPrime(100));
+
+// WAJP to print all the alternate Prime numbers up to a given range.
+function printAlternatePrimes(start, end) {
+    let count = 0;
+    if (start == 1 || start == 2) {
+        count++
+        console.log(2);
+    }
+    if (start % 2 == 0) {
+        start++;
+
+    }
+    for (let i = start; i <= end; i += 2) {
+        if (isPrime(i)) {
+            count++;
+            if (count % 2 != 0)
+                console.log(i);
+        }
+    }
+}
+
+// printAlternatePrimes(1, 30);
+
+// WAJP to print and count all prime numbers in a range where sum of digits is also prime number.
+function sumOfDigits(n) {
+    let sum = 0;
+    while (n>0) {
+        sum += (n%10);
+        n = Math.floor(n/10);
+    }
+    return sum;
+}
+
+function printAndCountPrimes(start, end) {
+    let count = 0;
+    if(start == 1 || start == 2) {
+        count++;
+        console.log(2);
+        start++;
+    }
+
+    if(start % 2 == 0) start++;
+
+    for (let i = start; i<=end; i+=2) {
+        if(isPrime(i) && isPrime(sumOfDigits(i))) {
+            count++;
+            console.log(i);
+        }
+    }
+    console.log("Count: ", count);
+}
+
+// printAndCountPrimes(1, 20);
+
+// WAJP to take user input and check the number is palindrome or not.
+function isPalindrome(n) {
+    let rev = 0;
+    const num = n;
+    while (n>0) {
+        rev = rev*10+(n%10);
+        n = Math.floor(n/10);
+    }
+
+    return rev == num;
+}
+
+// console.log(isPalindrome(121));
+
+// WAJP to print and count all the palindrome numbers in a given range.
+function printAndCountPalidromeInRange1(start, end) {
+    let count = 0;
+    for (let i = start; i<=end; i++) {
+        if(isPalindrome(i)) {
+            count++;
+            console.log(i);
+        }
+    }
+    console.log("Count: ", count);
+}
+
+// One small improvement we can skip the numbers with ending zeoroes they are never palindrome except 0.
+function printAndCountPalidromeInRange(start, end) {
+    let count = 0;
+    for (let i = start; i<=end; i++) {
+        if(i % 10 === 0 && i !== 0) continue;
+        if(isPalindrome(i)) {
+            count++;
+            console.log(i);
+        }
+    }
+    console.log("Count: ", count);
+}
+
+// printAndCountPalidromeInRange(1, 1000);
+
+// WAJP to take user input and print nth palindrome number.
+function printNthPalindrome(n) {
+    if(n<1) return;
+    let i = 0;
+    let count = 0;
+    while (true) {
+        if(isPalindrome(i)) {
+            count++;
+            if(count == n) {
+                console.log(i);
+                return;
+            }
+        }
+        i++;
+    }
+}
+
+printNthPalindrome(10);
