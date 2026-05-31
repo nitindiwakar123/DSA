d00dca8e95fe85263a17150a7a8cf15dbc461ad5
nitindiwakar123 <nitindiwakar2006@gmail.com>
2026-05-31 17:12:15 +0530

    add solution of 3100

---
+++ Leetcode/problem3100.js
@@ -0,0 +1,27 @@
+// Water Bottles II
+
+function maxBottlesDrunk(numBottles, numExchange) {
+    let maxBottlesDrunk = numBottles;
+    while (numBottles >= numExchange) {
+        maxBottlesDrunk += 1;
+        numBottles = numBottles-numExchange + 1;
+        numExchange++;
+    }
+
+    return maxBottlesDrunk;
+};
+
+// Test Cases
+// console.log(maxBottlesDrunk(13, 6));
+console.log(maxBottlesDrunk(10, 3));
+// console.log(maxBottlesDrunk(3, 2));
+// console.log(maxBottlesDrunk(3, 2));
+// console.log(maxBottlesDrunk(5, 2));
+// console.log(maxBottlesDrunk(5, 2));
+// console.log(maxBottlesDrunk(1, 2));
+// console.log(maxBottlesDrunk(2, 2));
+// console.log(maxBottlesDrunk(2, 3));
+// console.log(maxBottlesDrunk(3, 4));
+// console.log(maxBottlesDrunk(100, 100));
+
+// console.log(maxBottlesDrunk(1000, 2));
