e0bd0ed863ddaadf14739275a3d344a77bff05ec
nitindiwakar123 <nitindiwakar2006@gmail.com>
2026-06-11 08:04:11 +0530

    add problem1979

---
+++ Array/array2.js
@@ -0,0 +1,13 @@
+// WAP to swap two index values of the array.
+function swap(arr, a, b) {
+    const temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+
+    console.log(arr);
+    
+}
+
+// swap([1, 2, 3, 4, 5, 6], 1, 4);
+
+// 
