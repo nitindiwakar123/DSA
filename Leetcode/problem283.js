2b5e2e6956c7ca85ccf1b23bc45cb67a4b83d355
nitindiwakar123 <nitindiwakar2006@gmail.com>
2026-06-17 13:27:34 +0530

    add optimal 3 solution of problem283

--- Leetcode/problem283.js
+++ Leetcode/problem283.js
@@ -42,7 +42,7 @@
 //     return nums;
 // }
 
-// optimal
+// optimal 1
 // function moveZeroes(nums) {
 //     let j = -1;
 //     for (let i = 0; i < nums.length; i++) {
@@ -65,57 +65,42 @@
 //     return nums;
 // }
 
-// Test Cases
-// console.log(moveZeroes([0]));
-// console.log(moveZeroes([-41, 0, -66, 0, -78, 0, 79, 78]));
-
+// optimal 2
 // function moveZeroes(nums) {
-//     let j = -1;
-
+//     let nonzero = 0
 //     for (let i = 0; i < nums.length; i++) {
-//         if (nums[i] === 0) {
-//             j = i;
-//             break;
-//         }
-//     }
-
-//     if (j === -1) return nums;
-
-//     for (let i = j+1; i < nums.length; i++) {
-//         if(nums[i] != 0) {
-//             nums[j] = nums[i];
-//             nums[i] = 0;
-//             j++;
+//         if (nums[i] != 0) {
+//             const temp = nums[i];
+//             nums[i] = nums[nonzero];
+//             nums[nonzero] = temp;
+//             nonzero++;
 //         }
 //     }
 
 //     return nums;
 // }
 
+// optimal 3 - fast and slow pointer approach
 function moveZeroes(nums) {
-    let j = -1;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == 0) {
-            j = i;
-            break;
+    let slow = 0;
+
+    for (let fast = 0; fast < nums.length; fast++) {
+        if (nums[fast] != 0) {
+            nums[slow] = nums[fast];
+            slow++;
         }
     }
 
-    if (j == -1) return nums;
-
-    for (let i = j + 1; i < nums.length; i++) {
-        if (nums[i] != 0) {
-            nums[j] = nums[i];
-            nums[i] = 0;
-            j++;
-        }
+    for (let i = slow; i < nums.length; i++) {
+        nums[i] = 0;
     }
-
+    
     return nums;
 }
 
-// console.log(moveZeroes([1, 0, 2, 3, 2, 0, 0, 4, 5, 1]));
+console.log(moveZeroes([1, 0, 2, 3, 2, 0, 0, 4, 5, 1]));
 // console.log(moveZeroes([1, 1, 1]));
+// console.log(moveZeroes([1, 2, 3]));
 // console.log(moveZeroes([0, 0, 0]));
 // console.log(moveZeroes([0, 0, 0, 0, 0, 1, 2]));
 // console.log(moveZeroes([0, 0, 0, 0, 0, 1, 2, 0, 0]));
