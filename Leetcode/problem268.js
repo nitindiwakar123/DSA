6f8205e2e3d27c2ba29e73957f20fe3e095884ed
nitindiwakar123 <nitindiwakar2006@gmail.com>
2026-06-28 15:49:59 +0530

    add 4 approaches of problem268

--- Leetcode/problem268.js
+++ Leetcode/problem268.js
@@ -1,22 +1,57 @@
-// brute
+// brute - using hashing
+// function missingNumber(nums) {
+//     const freq = new Array(nums.length + 1).fill(0);
+
+//     for (const n of nums) {
+//         freq[n] = 1;
+//     }
+
+//     for (let i = 0; i < freq.length; i++) {
+//         if(freq[i] == 0)
+//             return i;
+//     }
+
+//     return -1;
+// }
+
+// approach 2 - using temp array
+// function missingNumber(nums) {
+//     const temp = new Array(nums.length + 1).fill(-1);
+
+//     for (const n of nums) {
+//         temp[n] = n;
+//     }
+
+//     for (let i = 0; i < temp.length; i++) {
+//         if (temp[i] == -1)
+//             return i;
+//     }
+
+//     return -1;
+// }
+
+// approah 3 - using XOR
+// function missingNumber(nums) {
+//     let res = 0;
+//     for (const n of nums) {
+//         res ^= n;
+//     }
+
+//     for (let i = 0; i <= nums.length; i++) {
+//         res ^= i;
+//     }
+
+//     return res;
+// }
+
+// Approach 4 - Sum of all elememnts
 function missingNumber(nums) {
-    const freq = new Array(nums.length + 1).fill(0);
+    const n = nums.length;
+    const sum1 = n * (n + 1) / 2;
+    const sum2 = nums.reduce((acc, ele) => acc + ele, 0);
 
-    for (const n of nums) {
-        freq[n] = 1;
-    }
-
-    for (let i = 0; i < freq.length; i++) {
-        if(freq[i] == 0)
-            return i;
-    }
-    
-    return -1;
+    return sum1 - sum2;
 }
-
-// Time & Space Complexity Analysis
-// brute - time - O(n+n) = O(n)
-//         space - S(n)
 
 // Test Cases
 // console.log(missingNumber([3, 0, 1]));
@@ -24,3 +59,15 @@ function missingNumber(nums) {
 // console.log(missingNumber([0,1,2,4,5,6,7,8,9]));
 // console.log(missingNumber([1,0,3,4]));
 // console.log(missingNumber([1]));
+
+// Time & Space Complexity Analysis
+// brute - time - O(n+n) = O(n)
+//         space - S(n)
+// approach 2 - time - O(n+n) = O(n)
+//              space - S(n)
+// approach 3 - time - O(n+n) = O(n)
+//              space - S(1)
+// approach 4 - time - O(n) = O(n)
+//              space - S(1)
+
+
