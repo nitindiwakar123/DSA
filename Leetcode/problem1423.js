14c7a69362ea96c2a6e4fa5fbf745a4524653dda
nitindiwakar123 <nitindiwakar2006@gmail.com>
2026-06-29 10:08:26 +0530

    add solution of problem1423

---
+++ Leetcode/problem1423.js
@@ -0,0 +1,29 @@
+function maxScore(cardPoints, k) {
+    const n = cardPoints.length;
+    let leftsum = 0;
+    let rightSum = 0;
+    let maxSum = 0;
+
+    for (let i = 0; i <= k - 1; i++) {
+        leftsum += cardPoints[i];
+        maxSum = leftsum;
+    }
+
+    let rightIndex = n - 1;
+    for (let i = k - 1; i >= 0; i--) {
+        leftsum = leftsum - cardPoints[i];
+        rightSum += cardPoints[rightIndex];
+        rightIndex--;
+
+        maxSum = Math.max(maxSum, leftsum + rightSum);
+    }
+
+    return maxSum;
+}
+
+
+// TC - O(2*k)
+// SC - O(1)
+
+// Test Cases
+console.log(maxScore([1, 2, 3, 4, 5, 6, 1], 3));
--- Leetcode/problem1539.js
+++ Leetcode/problem1539.js
@@ -73,8 +73,6 @@ function findKthPositive(arr, k) {
     return k;
 }
 
-
-
 // Test Cases
 // console.log(findKthPositive([2, 3, 4, 7, 11], 5));
 // console.log(findKthPositive([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 5));
