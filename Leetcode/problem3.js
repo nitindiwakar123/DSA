65d6d778e3ad001b00ebbbedf16284cdd4702cf0
nitindiwakar123 <nitindiwakar2006@gmail.com>
2026-06-30 10:13:02 +0530

    add optimal solution of problem3

--- Leetcode/problem3.js
+++ Leetcode/problem3.js
@@ -1,4 +1,29 @@
 // brute
+// time - O(n^2)
+// space - O(256)
+// It will not run on leetcode due to O(n^2) time complexity
+function lengthOfLongestSubstring(s) {
+    const n = s.length;
+    let maxLength = 0;
+
+    for (let i = 0; i < n; i++) {
+        let sub = "";
+        const freq = new Array(255).fill(0);
+        for (let j = i; j < n; j++) {
+            if (freq[s.charCodeAt(j)] != 0) break;
+            sub += s[j];
+            maxLength = Math.max(maxLength, j - i + 1);
+            freq[s.charCodeAt(j)] = 1;
+        } 
+    }
+
+    return maxLength;
+}
+
+// better
+// time - O(n+k)
+// space - O(1)
+
 function lengthOfLongestSubstring(s) {
     const n = s.length;
     let left = 0;
@@ -18,8 +43,34 @@ function lengthOfLongestSubstring(s) {
     return maxLen;
 }
 
+
+// optimal - with hashing
+// time - O(n)
+// space - Object(256)
+function lengthOfLongestSubstring(s) {
+    const n = s.length;
+    const hash = new Array(255).fill(-1);
+
+    let maxLength = 0;
+    let l = 0;
+    let r = 0;
+
+    while (r < n) {
+        if (hash[s.charCodeAt(r)] >= l && hash[s.charCodeAt(r)] != -1) {
+            l = hash[s.charCodeAt(r)] + 1;
+        }
+
+        maxLength = Math.max(maxLength, r - l + 1);
+
+        hash[s.charCodeAt(r)] = r;
+        r++;
+    }
+
+    return maxLength;
+}
+
 // Test Cases
-// console.log(lengthOfLongestSubstring("abcabcbb")); //3
+// console.log(lengthOfLongestSubstring("abcabcdebb")); //5
 // console.log(lengthOfLongestSubstring("bbbbb")); //1
 // console.log(lengthOfLongestSubstring("pwwkew")); //3
 // console.log(lengthOfLongestSubstring("")); //0
