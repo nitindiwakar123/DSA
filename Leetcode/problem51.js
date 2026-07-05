cc722b674927cad72e0b0c66879931e730225391
nitindiwakar123 <nitindiwakar2006@gmail.com>
2026-07-05 15:27:50 +0530

    add problem52 sol

--- Leetcode/problem51.js
+++ Leetcode/problem51.js
@@ -32,26 +32,58 @@ function isSafe(col, row, board, n) {
     return true;
 }
 
-function backtrack(col, board, ans, n) {
+// function backtrack(col, board, ans, n) {
+//     if (col == n) {
+//         ans.push(board.map(r => r.join("")));
+//         return
+//     }
+
+//     for (let row = 0; row < n; row++) {
+//         if (!(isSafe(col, row, board, n))) continue;
+
+//         board[row][col] = "Q";
+//         backtrack(col + 1, board, ans, n);
+//         board[row][col] = ".";
+//     }
+// }
+
+// function solveNQueens(n) {
+//     const ans = [];
+//     const board = Array.from({ length: n }, () => Array(n).fill('.'));
+//     backtrack(0, board, ans, n);
+//     return ans;
+// }
+
+
+// optimal with hashing
+function backtrack(col, board, ans, leftRow, lowerDiagonal, upperDiagonal, n) {
     if (col == n) {
         ans.push(board.map(r => r.join("")));
         return
     }
 
     for (let row = 0; row < n; row++) {
-        if (!(isSafe(col, row, board, n))) continue;
+        if (leftRow[row] == 1 || lowerDiagonal[row + col] == 1 || upperDiagonal[n - 1 + col - row] == 1) continue;
 
         board[row][col] = "Q";
-        backtrack(col + 1, board, ans, n);
+        leftRow[row] = 1;
+        lowerDiagonal[row + col] = 1;
+        upperDiagonal[n - 1 + col - row] = 1;
+        backtrack(col + 1, board, ans, leftRow, lowerDiagonal, upperDiagonal, n);
         board[row][col] = ".";
+        leftRow[row] = 0;
+        lowerDiagonal[row + col] = 0;
+        upperDiagonal[n - 1 + col - row] = 0;
     }
 }
 
 function solveNQueens(n) {
     const ans = [];
     const board = Array.from({ length: n }, () => Array(n).fill('.'));
-    backtrack(0, board, ans, n);
+    const leftRow = new Array(n).fill(0);
+    const lowerDiagonal = new Array(2 * n - 1).fill(0);
+    const upperDiagonal = new Array(2 * n - 1).fill(0);
+    backtrack(0, board, ans, leftRow, lowerDiagonal, upperDiagonal, n);
     return ans;
 }
-
 console.log(solveNQueens(4));
